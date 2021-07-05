9e02d188080a342f8bb82a53ce7eb3c0e47fe1dd
Tima Nlemvo <tnlemvo@AML-TNLEMVO.caa.comgit commit -m added>
2021-07-04 17:49:50 -0700

    updated server.js file.

--- server.js
+++ server.js
@@ -1,21 +1,74 @@
 const express = require("express");
-const apiRoutes = require("./routes/apiRoutes");
-const htmlRoutes = require("./routes/htmlRoutes");
-
-// Initialize the app and create a port
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Set up body parsing, static, and route middleware
+let notesData = [];
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
-app.use("/api", apiRoutes);
-app.use("/", htmlRoutes);
+app.use(express.static(path.join(__dirname, "Develop/public")));
+
+// api call response
+app.get("/api/notes", function(err, res) {
+    try {
+      // reads the notes from json file
+      notesData = fs.readFileSync("Develop/db/db.json", "utf8");
+      // parse it so notesData is an array of objects
+      notesData = JSON.parse(notesData);
+  
+      // error handling
+    } catch (err) {
+      console.log("\n error (in app.get.catch):");
+      console.log(err);
+    }
+    //   send objects to the browser
+    res.json(notesData);
+  });
+  
+  // writes the new note to the json file
+  app.post("/api/notes", function(req, res) {
+    try {
+      // reads the json file
+      notesData = fs.readFileSync("./Develop/db/db.json", "utf8");
+      console.log(notesData);
+  
+      // parse the data to get an array of objects
+      notesData = JSON.parse(notesData);
+      // add the new note to the array of note objects
+      notesData.push(req.body); // req.body - user input
+      // make it string(stringify)so you can write it to the file
+      notesData = JSON.stringify(notesData);
+      // writes the new note to file
+      fs.writeFile("./Develop/db/db.json", notesData, "utf8", function(err) {
+        // error handling
+        if (err) throw err;
+      });
+      // changeit back to an array of objects & send it back to the browser(client)
+      res.json(JSON.parse(notesData));
+  
+      // error Handling
+    } catch (err) {
+      throw err;
+      console.error(err);
+    }
+  });
+
+// HTML GET Requests
+
+// Web page when the Get started button is clicked
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
+});
+
+// If no matching route is found default to home
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
+});
 
 // Start the server on the port
-
 app.listen(PORT, function() {
   console.log("SERVER IS LISTENING: " + PORT);
-})
+});
