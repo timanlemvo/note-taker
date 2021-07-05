908b71dcdea3418d2fb6df23bf86bb3511a9ce6b
Tima Nlemvo <tnlemvo@AML-TNLEMVO.caa.comgit commit -m added>
2021-07-04 19:44:11 -0700

    added heroku

--- server.js
+++ server.js
@@ -37,6 +37,7 @@ app.get("/api/notes", function(err, res) {
   
       // parse the data to get an array of objects
       notesData = JSON.parse(notesData);
+      req.body.id = notesData.length;
       // add the new note to the array of note objects
       notesData.push(req.body); // req.body - user input
       // make it string(stringify)so you can write it to the file
@@ -56,19 +57,54 @@ app.get("/api/notes", function(err, res) {
     }
   });
 
+// -----BONUS--------- Note Deletion
+
+app.delete("/api/notes/:id", function(req, res) {
+    try {
+      //  reads the json file
+      notesData = fs.readFileSync("Develop/db/db.json", "utf8");
+      // parse the data to get an array of the objects
+      notesData = JSON.parse(notesData);
+      // delete the old note from the array on note objects
+      notesData = notesData.filter(function(note) {
+        return note.id != req.params.id;
+      });
+      // make it string(stringify)so you can write it to the file
+      notesData = JSON.stringify(notesData);
+      // write the new notes to the file
+      fs.writeFile("Develop/db/db.json", notesData, "utf8", function(err) {
+        // error handling
+        if (err) throw err;
+      });
+  
+      // change it back to an array of objects & send it back to the browser (client)
+      res.send(JSON.parse(notesData));
+  
+      // error handling
+    } catch (err) {
+      throw err;
+      console.log(err);
+    }
+  });
+
+
 // HTML GET Requests
 
 // Web page when the Get started button is clicked
 app.get("/notes", function(req, res) {
-  res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
-});
-
+    res.sendFile(path.join(__dirname, "Develop/public/notes.html"));
+  });
+  
 // If no matching route is found default to home
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
-});
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "Develop/public/index.html"));
+  });
 
-// Start the server on the port
+app.get("/api/notes", function(req, res) {
+    return res.sendFile(path.json(__dirname, "Develop/db/db.json"));
+  });
+
+  // Start the server on the port
 app.listen(PORT, function() {
   console.log("SERVER IS LISTENING: " + PORT);
 });
